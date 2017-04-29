24cc204ddbb5b6525a8f90030a8866db82b72ed3
Joe Sewell <Joe-K-Sewell@users.noreply.github.com>
2017-04-29 14:35:41 -0400

    Attempt to make an xUnit test project

---
+++ IncognitumUnitTests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace IncognitumUnitTests
+{
+    public class Class1
+    {
+        [Theory]
+        [InlineData("Yo")]
+        public void BasicTest(String it)
+        {
+            var x = new Incognitum.Connections.HttpsConnection("mastodon.cloud");
+        }
+    }
+}
